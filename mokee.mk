e2c57f007d6bb2ef91f7558182d31ce236f28385
martincz <martincz@mokeedev.com>
2017-02-03 01:27:03 +0800

    h815: mokee-ify
    Change-Id: Ifcb2b1ee31facd5285bcacb9cf2f1a6632ccb464

--- mokee.mk
+++ mokee.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/lge/h815/full_h815.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_h815
+PRODUCT_NAME := mk_h815
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g4" \
